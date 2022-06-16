dc1e6a8f4c2e061af2d0c5566ee01426fc97207b
Angela Hanshaw <angela.hanshaw@gmail.com>
2022-06-15 19:58:54 -0500

    update post fields

--- admin/preview-templates/post.js
+++ admin/preview-templates/post.js
@@ -5,44 +5,24 @@ const html = htm.bind(h);
 
 // Preview component for a Post
 const Post = createClass({
-  render() {
-    const entry = this.props.entry;
+	render() {
+		const entry = this.props.entry;
 
-    return html`
-      <main>
-        <article>
-          <h1>${entry.getIn(["data", "title"], null)}</h1>
-          <p>
-            <small>
-              <time
-                >${
-                  format(
-                    entry.getIn(["data", "date"], new Date()),
-                    "dd MMM, yyyy"
-                  )
-                }</time
-              >
-              ${" by Author"}
-            </small>
-          </p>
-
-          <p>${entry.getIn(["data", "summary"], "")}</p>
-
-          ${this.props.widgetFor("body")}
-          <p>
-            ${
-              entry.getIn(["data", "tags"], []).map(
-                tag =>
-                  html`
-                    <a href="#" rel="tag">${tag}</a>
-                  `
-              )
-            }
-          </p>
-        </article>
-      </main>
-    `;
-  }
+		return html`
+			<main>
+				<article>
+					<div>
+						<img
+							src="${entry.getIn(["data", "image"], null)}"
+							alt="${entry.getIn(["data", "alt"], null)}"
+						/>
+					</div>
+					<h1>${entry.getIn(["data", "title"], null)}</h1>
+					${this.props.widgetFor("body")}
+				</article>
+			</main>
+		`;
+	},
 });
 
 export default Post;
